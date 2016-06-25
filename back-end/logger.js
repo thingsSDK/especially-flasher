93344c154834b5e9540c810a9e9afcf07792ee2e
Andrew Chalkley <andrew@chalkley.org>
2016-06-24 22:16:29 -0700

    Removed logger hack. Latest versions fixed it

--- back-end/logger.js
+++ back-end/logger.js
@@ -2,10 +2,6 @@
 
 const bunyan = require("bunyan");
 
-//https://github.com/electron/electron/issues/2033#issuecomment-199263926
-process.stderr.write = console.error.bind(console); 
-process.stdout.write = console.log.bind(console); 
-
 module.exports = bunyan.createLogger({
     name: "flasher.js"
 });
