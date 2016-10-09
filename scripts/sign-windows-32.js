e067536dd593486431b873b62206919982ad83c5
Andrew Chalkley <andrew@chalkley.org>
2016-10-09 09:46:12 -0700

    Adding 32bit support

---
+++ scripts/sign-windows-32.js
@@ -0,0 +1,21 @@
+"use strict";
+const fs = require("fs");
+const path = require('path');
+
+const rootPath = path.join(__dirname, '..');
+const outPath = path.join(rootPath, 'out');
+const packedPath = path.join(outPath, 'flasher.js-win32-ia32');
+const exec = require('child_process').execSync;
+
+fs.readdir(packedPath, (err, files) => {
+    const dllsAndExes = files.filter(file => {
+        return file.endsWith(".dll") || file.endsWith(".exe");
+    });
+    dllsAndExes.forEach(run);
+});
+
+
+function run(file) {
+    exec(`signtool.exe sign /a /d "Flasher.js" /du https://github.com/ThingsSDK/flasher.js /s MY /n "Andrew Chalkley" /fd sha1 /t http://timestamp.verisign.com/scripts/timstamp.dll /v "${path.join(packedPath, file)}"`);
+    exec(`signtool.exe sign /a /d "Flasher.js" /du https://github.com/ThingsSDK/flasher.js /s MY /n "Andrew Chalkley" /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /td sha256 /as /v "${path.join(packedPath, file)}"`);
+}
---
+++ scripts/windows-installer-32.js
@@ -0,0 +1,41 @@
+
+"use strict";
+//Taken from Electron API Demos
+//https://github.com/electron/electron-api-demos/blob/master/script/installer.js
+
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller;
+const path = require('path');
+const rimraf = require('rimraf');
+
+deleteOutputFolder()
+  .then(getInstallerConfig)
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error);
+    process.exit(1);
+  });
+
+function getInstallerConfig() {
+  const rootPath = path.join(__dirname, '..');
+  const outPath = path.join(rootPath, 'out');
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'flasher.js-win32-ia32'),
+    iconUrl: 'https://raw.githubusercontent.com/thingssdk/flasher.js/resources/icon.ico', 
+    loadingGif: path.join(rootPath, 'resources', 'loading.gif'),
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'installers'),
+    setupExe: 'flasher.js-setup.exe',
+    setupIcon: path.join(rootPath, 'resources', 'icon.ico'),
+    skipUpdateIcon: true
+  });
+}
+
+function deleteOutputFolder() {
+  return new Promise((resolve, reject) => {
+    rimraf(path.join(__dirname, '..', 'out', 'installers'), error => {
+      if (error) reject(error);
+      else resolve();
+    });
+  });
+}
