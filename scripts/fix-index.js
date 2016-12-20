d2faa0d35fe91e424db62c2af7216ea84c9976f8
Andrew Chalkley <andrew@chalkley.org>
2016-12-19 22:36:37 -0800

    Version bump of rom-comm. build process complete

--- scripts/fix-index.js
+++ scripts/fix-index.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
 const path = require('path');
-
+const { openAndFix } = require('./helpers');
 const rootPath = path.join(__dirname, '..');
+const pathToIndex = path.join(rootPath, 'front-end', 'index.html');
+
+console.log('Fixing UI...');
+const fileContents = openAndFix(pathToIndex, 'utf8');
+fs.writeFileSync(pathToIndex, fileContents);
+console.log('...fixed');
